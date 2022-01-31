3edf5db976501ea8dc33774adc56070112ae96f2
PREMSAGAR KUSHWAHA <67200446+premsagarkushwaha@users.noreply.github.com>
2022-01-31 21:06:37 +0530

    Create rec5.c

---
+++ Recursion/rec5.c
@@ -0,0 +1,27 @@
+/* Write a program in C to count the digits of a given number using recursion. Go to the editor
+Test Data :
+Input a number : 50
+Expected Output :
+
+The number of digits in the number is :  2*/
+
+#include<stdio.h>
+
+int callrec(int a)
+{
+	if(a==0)
+	{
+		return 0;
+	}
+	else return 1+callrec(a/10);
+}
+
+int main()
+{
+	printf("enter any number between we need to print matural number: ");
+	int d;
+	scanf("%d",&d);
+	printf("no. of digit %d",callrec(d));
+	
+	
+}
